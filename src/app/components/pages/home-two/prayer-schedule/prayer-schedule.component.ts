0bac75bd10910417348e319d90ad163eb4f7b464
Mohammad Chohan <mohammadchohan6f@gmail.com>
2023-05-29 21:39:11 +1000

    fix bug in timeToFloat and clean up code

--- src/app/components/pages/home-two/prayer-schedule/prayer-schedule.component.ts
+++ src/app/components/pages/home-two/prayer-schedule/prayer-schedule.component.ts
@@ -1,116 +1,83 @@
-import {Component, OnInit} from '@angular/core';
-import {
-  PrayerTimesService
-} from '../../../../prayer-times/prayer-times.service';
+import { Component } from '@angular/core';
+import { PrayerTimesService } from '../../../../prayer-times/prayer-times.service';
 import { FirestoreService } from 'src/app/firestore/firestore.service';
 
-
-type OutputPrayerTimesNames = 'fajr' | 'sunrise' | 'dhuhr' | 'asr' | 'maghrib' | 'isha'
+type OutputPrayerNames =
+  | 'fajr'
+  | 'sunrise'
+  | 'dhuhr'
+  | 'asr'
+  | 'maghrib'
+  | 'isha';
 
 type OutputPrayer = {
-  name: OutputPrayerTimesNames,
-  athan: string,
-  iqamah: string
-}
+  name: OutputPrayerNames;
+  athan: string;
+  iqamah: string;
+};
 
-type OutputPrayerTimes = OutputPrayer[]
-
-export type HardcodedIqamahTimes = {
-    [name in OutputPrayerTimesNames]?: string;
-}
+export type HardcodedIqamahTimes = Partial<Record<OutputPrayerNames, string>>;
 
 /** Number of minutes from athan to iqamah */
 export type IqamahOffset = {
-    [name in OutputPrayerTimesNames]?: number
-}
+  [name in OutputPrayerNames]?: number;
+};
 
 @Component({
   selector: 'app-prayer-schedule',
   templateUrl: './prayer-schedule.component.html',
   styleUrls: ['./prayer-schedule.component.css'],
 })
-export class PrayerScheduleComponent implements OnInit {
-  private outputPrayerNames: Set<OutputPrayerTimesNames> = new Set(['fajr', 'sunrise', 'dhuhr', 'asr', 'maghrib', 'isha'])
+export class PrayerScheduleComponent {
+  private static AM_SUFFIX = 'am' as const;
+  private static PM_SUFFIX = 'pm' as const;
+  private static outputPrayerNames = [
+    'fajr',
+    'sunrise',
+    'dhuhr',
+    'asr',
+    'maghrib',
+    'isha',
+  ] as const;
 
-  private readonly prayTimes
-  //@ts-ignore
-  public outputTimes: OutputPrayerTimes
-  public today: Date = new Date()
+  public outputTimes!: OutputPrayer[];
+  public today: Date = new Date();
 
-  //@ts-ignore
-  private offset: IqamahOffset
+  private readonly prayTimes;
+  private offset!: IqamahOffset;
+  private hardcodedTimes!: HardcodedIqamahTimes;
 
-  //@ts-ignore
-  private hardcodedTimes: HardcodedIqamahTimes
+  constructor(
+    prayerTimesService: PrayerTimesService,
+    firestoreServce: FirestoreService
+  ) {
+    this.prayTimes = prayerTimesService.getTimes(
+      this.today,
+      [-37.8226, 145.0354],
+      'auto',
+      'auto',
+      'Float'
+    );
 
-  private AM_SUFFIX = 'am' as const
-  private PM_SUFFIX = 'pm' as const
-
-
-  constructor(prayerTimesService: PrayerTimesService, firestoreServce: FirestoreService) {
-    this.prayTimes = prayerTimesService.getTimes(this.today, [-37.8226, 145.0354], 'auto', 'auto', 'Float')
-    firestoreServce.getIqamahOffsets().then((offsets) => {
-      this.offset = offsets;
-      firestoreServce.getHardcodedTimes().then(hardcodedTimes => {
-        this.hardcodedTimes = hardcodedTimes;
-        this.outputTimes = this.getIqamahTimes();
+    firestoreServce
+      .getIqamahOffsets()
+      .then((offsets) => {
+        this.offset = offsets;
+        firestoreServce.getHardcodedTimes().then((hardcodedTimes) => {
+          this.hardcodedTimes = hardcodedTimes;
+          this.outputTimes = this.getIqamahTimes();
+        });
       })
-    })
+      .catch((a) => console.log(a));
   }
 
-  private getIqamahTimes(): OutputPrayerTimes {
-    const output = Object.entries(this.prayTimes)
-      .filter(([k, v]) => this.outputPrayerNames.has(k as any))
-      .map(([k, v]) => ({
-            name: k as OutputPrayerTimesNames,
-            athan: this.to12HourFormat(v),
-            iqamah: this.getIqamahTime(k as OutputPrayerTimesNames)
-        }))
-
-    return output
-  }
-
-  private getIqamahTime(timeName: OutputPrayerTimesNames): string {
-    if (timeName in this.hardcodedTimes) return this.hardcodedTimes[timeName]!
-
-    let time = this.prayTimes[timeName]
-    let offset = this.offset[timeName]
-    if (offset !== undefined) {
-      time += offset / 60
-    }
-    return this.to12HourFormat(time)
-  }
-
-  private to12HourFormat(time: number): string {
-    let minutes = Math.round((time % 1) * 60)
-    let hours = (Math.floor(time) + Math.floor(minutes / 60)) % 24
-    minutes %= 60
-    let suffix = (hours < 12) ? this.AM_SUFFIX : this.PM_SUFFIX
-    hours = (hours % 12) || 12
-    return this.leftFill2Digits(hours) + ':' + this.leftFill2Digits(minutes) + ' ' + suffix
-  }
-
-  private leftFill2Digits(num: number): string {
-    return num.toString().padStart(2, '0');
-  }
-
-
-
-
-  ngOnInit(): void {
-    console.log(this.prayTimes);
-    console.log(this.outputTimes);
-  }
-
-
-  getNextPrayer() {
-    let currentDate = this.today
+  getNextPrayer(): OutputPrayerNames {
+    let currentDate = this.today;
     let currentTime = currentDate.getHours() + currentDate.getMinutes() / 60;
     let prevTime = 24;
-    let nextPrayer = 'fajr';
+    let nextPrayer: OutputPrayerNames = 'fajr';
     for (const prayer of this.outputTimes) {
-      let prayerTime = prayer.athan
-      let time = this.timeToFloat(prayerTime);
+      let time = this.timeToFloat(prayer.athan);
       if (time >= currentTime && time < prevTime) {
         prevTime = time;
         nextPrayer = prayer.name;
@@ -123,9 +90,49 @@ export class PrayerScheduleComponent implements OnInit {
    * @param time string in "hh:mm am/pm format, e.g. "09:34 pm"
    */
   timeToFloat(time: string) {
-    let [timeArray, suffix] = time.split(' ')
-    let [hours, minutes] = timeArray.split(':').map(x => +x)
-    hours += suffix === this.PM_SUFFIX ? 12 : 0
+    let [noSuffixTime, suffix] = time.split(' ');
+    let [hours, minutes] = noSuffixTime.split(':').map((x) => +x);
+    hours %= 12;
+    hours +=
+      suffix.toLowerCase() === PrayerScheduleComponent.PM_SUFFIX ? 12 : 0;
     return hours + minutes / 60;
+  }
+
+  private getIqamahTimes(): OutputPrayer[] {
+    return PrayerScheduleComponent.outputPrayerNames.map((prayer) => ({
+      name: prayer,
+      athan: this.to12HourFormat(this.prayTimes[prayer]),
+      iqamah: this.getIqamahTime(prayer),
+    }));
+  }
+
+  private getIqamahTime(timeName: OutputPrayerNames): string {
+    let hardcodedTime = this.hardcodedTimes[timeName];
+    if (hardcodedTime !== undefined) {
+      return hardcodedTime;
+    }
+
+    let time = this.prayTimes[timeName];
+    let offset = this.offset[timeName];
+    if (offset !== undefined) {
+      time += offset / 60;
+    }
+    return this.to12HourFormat(time);
+  }
+
+  private to12HourFormat(time: number): string {
+    let minutes = Math.round((time % 1) * 60);
+    let hours = (Math.floor(time) + Math.floor(minutes / 60)) % 24;
+    minutes %= 60;
+    let suffix =
+      hours < 12
+        ? PrayerScheduleComponent.AM_SUFFIX
+        : PrayerScheduleComponent.PM_SUFFIX;
+    hours = hours % 12 || 12;
+    return this.to2Digits(hours) + ':' + this.to2Digits(minutes) + ' ' + suffix;
+  }
+
+  private to2Digits(num: number): string {
+    return num.toString().padStart(2, '0');
   }
 }
