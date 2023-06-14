b25cc72695b8ee5027f3abf3ca9c4a7d157517e4
Mohammad Chohan <mohammadchohan6f@gmail.com>
2023-06-15 04:42:13 +0900

    remove *.spec.ts files

--- src/app/components/helper/shop/product-service-helper.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ProductServiceHelper } from './product-service-helper';
-
-describe('ProductServiceHelper', () => {
-  it('should create an instance', () => {
-    expect(new ProductServiceHelper()).toBeTruthy();
-  });
-});
